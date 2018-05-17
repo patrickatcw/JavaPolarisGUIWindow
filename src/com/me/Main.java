92de935fc6df38b05af09a4c7cb3300cf8b932f5
patrickatcw <pj8392@yahoo.com>
2018-05-16 20:35:26 -0400

    added references to do first example

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -1,6 +1,8 @@
 package com.me;
 
 //https://www.youtube.com/watch?v=5vSyylPPEko
+//https://www.jetbrains.com/help/idea/placing-gui-components-on-a-form.html
+//https://www.jetbrains.com/help/idea/moving-components.html
 
 public class Main {
 
